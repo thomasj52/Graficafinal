4757ba3f2726db9d696c57973e283e2b293da09f
Augusto3737 <augustogalban@hotmail.com>
2026-07-16 02:32:12 -0300

    Portal Ciudad Terminado por ahora

--- Assets/ProyectoIntegrador/Portal-Ciudad/SceneLoader.cs
+++ Assets/ProyectoIntegrador/Portal-Ciudad/SceneLoader.cs
@@ -7,4 +7,26 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("PI_Ciudad_07", LoadSceneMode.Additive);
     }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "PI_Ciudad_07")
+        {
+            foreach (AudioListener al in FindObjectsOfType<AudioListener>())
+            {
+                if (al.gameObject.scene.name == "PI_Ciudad_07")
+                    al.enabled = false;
+            }
+        }
+    }
 }
