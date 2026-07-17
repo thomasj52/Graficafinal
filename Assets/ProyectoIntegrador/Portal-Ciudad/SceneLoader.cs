b5d651530f0f46903761c88cf2f20914ba43ce3f
Iarovich <iaradelacasa2006@gmail.com>
2026-07-17 13:39:03 -0300

    a

--- Assets/ProyectoIntegrador/Portal-Ciudad/SceneLoader.cs
+++ Assets/ProyectoIntegrador/Portal-Ciudad/SceneLoader.cs
@@ -5,9 +5,7 @@ public class SceneLoader : MonoBehaviour
 {
     void Start()
     {
-        // Cargamos ambas escenas en paralelo de forma aditiva
         SceneManager.LoadScene("PI_Ciudad_07", LoadSceneMode.Additive);
-        SceneManager.LoadScene("PI_Carta-Barco_03", LoadSceneMode.Additive);
     }
 
     void OnEnable()
@@ -22,16 +20,12 @@ public class SceneLoader : MonoBehaviour
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // Si se carga la ciudad o la escena del barco, limpiamos sus AudioListeners
-        if (scene.name == "PI_Ciudad_07" || scene.name == "PI_Carta-Barco_03")
+        if (scene.name == "PI_Ciudad_07")
         {
             foreach (AudioListener al in FindObjectsOfType<AudioListener>())
             {
-                // Desactivamos el AudioListener si pertenece a cualquiera de las dos escenas cargadas
-                if (al.gameObject.scene.name == scene.name)
-                {
+                if (al.gameObject.scene.name == "PI_Ciudad_07")
                     al.enabled = false;
-                }
             }
         }
     }
